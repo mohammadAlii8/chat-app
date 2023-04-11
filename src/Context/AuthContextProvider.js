36264b5ae96d76e06c1964c26c73e4c493b5c890
MohammadAli <galaxy.mrz8@gmail.com>
2023-04-12 01:08:11 +0430

    Context Created

---
+++ src/Context/AuthContextProvider.js
@@ -0,0 +1,30 @@
+import React, { Children, createContext, useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { auth } from '../firebase';
+
+const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(false)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            setUser(user)
+            setLoading(false)
+            if (user) navigate("/cahts")
+        })
+
+    }, [user, navigate])
+    return (
+        <div>
+            <AuthContext.Provider value={user}>
+                {!loading && children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthContextProvider;
