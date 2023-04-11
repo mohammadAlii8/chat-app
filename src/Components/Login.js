680fdafaac825c85650f4bb76757f20df717e6a3
MohammadAli <galaxy.mrz8@gmail.com>
2023-04-11 20:50:28 +0430

    Styles Added to PRJ

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import google from "../assets/icons8-google.svg"
-
+import styles from "./Login.module.css"
 const Login = () => {
     return (
-        <div>
-            <div>
+        <div className={styles.loginPage}>
+            <div className={styles.loginCard}>
                 <h2>Welcome to GalaxyGram</h2>
-                <div>
+                <div className={styles.button}>
                     <img src={google} alt="google" />Sign in with Google
                 </div>
             </div>
